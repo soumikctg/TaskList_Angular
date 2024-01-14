474c76b2f827c0b9086bbe3849fe2c1a790784b9
soumikctg <soumikctg616@gmail.com>
2024-01-14 21:12:26 +0600

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeadSectionComponent } from './head-section/head-section.component';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeadSectionComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FirstAngular';
+
 }
---
+++ src/app/head-section/head-section.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-head-section',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './head-section.component.html',
+  styleUrl: './head-section.component.css'
+})
+export class HeadSectionComponent {
+  title = 'Task Tracker';
+
+  toggleAddTask(){
+    console.log('toggle')
+  }
+}
